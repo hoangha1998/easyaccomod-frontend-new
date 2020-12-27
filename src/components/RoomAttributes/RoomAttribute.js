fe6f20c697019a821dd4a31ffd03a40818ed034d
Hoang Thai Ha <hoangha1998.tb@gmai.com>
2020-12-27 22:21:23 +0700

    fix view room attribute

--- src/components/RoomAttributes/RoomAttribute.js
+++ src/components/RoomAttributes/RoomAttribute.js
@@ -3,7 +3,9 @@ import {ATTRIBUTE_VALUE_TYPE} from '../../common/constants';
 
 const RoomAttribute = React.memo(function RoomAttribute({attribute}) {
   let value = '';
-  switch (attribute.value_type) {
+
+
+  switch (attribute.attribute_value_type) {
     case ATTRIBUTE_VALUE_TYPE.INTEGER:
       value = attribute.int_value;
       break;
